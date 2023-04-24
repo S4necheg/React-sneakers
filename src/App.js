b2f60062a2076dabfba7536267749eb7a2e7aa3f
S4necheg <a.muxanin@mail.ru>
2023-04-24 20:51:01 +0300

    test-commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import Card from './components/Card';
+import Header from './components/Header';
+import Drawer from './components/Drawer';
+
+const arr = [
+  { title: 'Мужские Кроссовки Nike Blazer Mid Suede', price: '12 999', imageUrl:'/img/sneakers/1.jpg'},
+  { title: 'Мужские Кроссовки Nike Air Max 270', price: '12 999', imageUrl: '/img/sneakers/2.jpg'},
+  { title: 'Мужские Кроссовки Nike Blazer Mid Suede', price: '8 499', imageUrl: '/img/sneakers/3.jpg'},
+  { title: 'Кроссовки Puma X Aka Boku Future Rider', price: '8 999', imageUrl: '/img/sneakers/4.jpg'}
+];
+
+function App() {
+  return (
+    <div className="wrapper clear">
+      <Drawer />
+      <Header />
+
+      <div className="content p-40">
+        <div className="d-flex mb-40">
+          <img width={960} height={300} src="/img/slider1.svg" alt="Slider"/>
+        </div>
+        <div className="d-flex align-center justify-between mb-40 pr-30">
+          <h1>Все кроссовки</h1>
+          <div className="search-block d-flex">
+            <img src="/img/search.svg" alt="Search" />
+            <input placeholder="Поиск ..." />
+          </div>
+        </div>
+
+        <div className="d-flex">
+          {arr.map((obj) => (
+            <Card 
+              title={obj.title}
+              price={obj.price}
+              imageUrl={obj.imageUrl}
+              onClick={() => console.log(obj)}
+            />
+          ))}
+        </div> 
+      </div>
+    </div>
+  );
+}
+
+export default App;
