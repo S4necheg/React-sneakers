605c22c2d5e8c8230f089b0227fada9572198155
S4necheg <a.muxanin@mail.ru>
2023-04-25 14:38:54 +0300

    add usestate to button cart

--- src/App.js
+++ src/App.js
@@ -11,10 +11,12 @@ const arr = [
 ];
 
 function App() {
+  const [cartOpened, setCartOpened] = React.useState(false);
+
   return (
     <div className="wrapper clear">
-      <Drawer />
-      <Header />
+      {cartOpened && <Drawer onClose={() => setCartOpened(false)} />}
+      <Header onClickCart={() => setCartOpened(true)} />
 
       <div className="content p-40">
         <div className="d-flex mb-40">
