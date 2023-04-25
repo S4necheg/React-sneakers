54263eccdafa73e0055776484dd0e80de172f96f
S4necheg <a.muxanin@mail.ru>
2023-04-25 16:50:52 +0300

    create db and join to app

--- src/App.js
+++ src/App.js
@@ -3,15 +3,24 @@ import Card from './components/Card';
 import Header from './components/Header';
 import Drawer from './components/Drawer';
 
-const arr = [
-  { title: 'Мужские Кроссовки Nike Blazer Mid Suede', price: '12 999', imageUrl:'/img/sneakers/1.jpg'},
-  { title: 'Мужские Кроссовки Nike Air Max 270', price: '12 999', imageUrl: '/img/sneakers/2.jpg'},
-  { title: 'Мужские Кроссовки Nike Blazer Mid Suede', price: '8 499', imageUrl: '/img/sneakers/3.jpg'},
-  { title: 'Кроссовки Puma X Aka Boku Future Rider', price: '8 999', imageUrl: '/img/sneakers/4.jpg'}
-];
+// const arr = [
+//   { title: 'Мужские Кроссовки Nike Blazer Mid Suede', price: '12 999', imageUrl:'/img/sneakers/1.jpg'},
+//   { title: 'Мужские Кроссовки Nike Air Max 270', price: '12 999', imageUrl: '/img/sneakers/2.jpg'},
+//   { title: 'Мужские Кроссовки Nike Blazer Mid Suede', price: '8 499', imageUrl: '/img/sneakers/3.jpg'},
+//   { title: 'Кроссовки Puma X Aka Boku Future Rider', price: '8 999', imageUrl: '/img/sneakers/4.jpg'}
+// ];
 
 function App() {
+  const [items, setItems] = React.useState([]);
   const [cartOpened, setCartOpened] = React.useState(false);
+
+  React.useEffect(() => {
+    fetch('http://localhost:3001/items').then((res) => {
+      return res.json();
+    }).then((json) => {
+      setItems(json);
+    });
+  }, []);
 
   return (
     <div className="wrapper clear">
@@ -30,8 +39,8 @@ function App() {
           </div>
         </div>
 
-        <div className="d-flex">
-          {arr.map((obj) => (
+        <div className="d-flex flex-wrap">
+          {items.map((obj) => (
             <Card 
               title={obj.title}
               price={obj.price}
